bf714c924949c59ad6a1fed9d0ba2a08523a3114
Aditya Mehta <aditya0mehta1@gmail.com>
2024-10-29 22:06:15 +0530

    clodinary and multer setup is done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,77 @@
+import { v2 as cloudinary } from "cloudinary";
+
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+    }
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //file has been uploaded succefull
+    console.log("file is uploaded on cloudinary", response.url);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove the locally saved tempory file as the upload operation failed
+  }
+};
+
+export { uploadOnCloudinary };
+
+/*   
+
+(async function () {
+  // Configuration
+
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET, // Click 'View API Keys' above to copy your API secret
+  });
+
+  // Upload an image
+  const uploadResult = await cloudinary.uploader
+    .upload(
+      "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+      {
+        public_id: "shoes",
+      }
+    )
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(uploadResult);
+
+  // Optimize delivery by resizing and applying auto-format and auto-quality
+  const optimizeUrl = cloudinary.url("shoes", {
+    fetch_format: "auto",
+    quality: "auto",
+  });
+
+  console.log(optimizeUrl);
+
+  // Transform the image: auto-crop to square aspect_ratio
+  const autoCropUrl = cloudinary.url("shoes", {
+    crop: "auto",
+    gravity: "auto",
+    width: 500,
+    height: 500,
+  });
+
+  console.log(autoCropUrl);
+})();
+
+
+*/

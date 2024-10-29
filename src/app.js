62f5cbe7e26e6a82820b92f0d5e228e1ea4d0e68
Aditya Mehta <aditya0mehta1@gmail.com>
2024-10-29 16:03:03 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "process.env.CORS_ORIGIN",
+    credentials: true,
+  })
+);
+
+//form se data liye h
+app.use(express.json({ limit: "16kb" }));
+
+//url se data le rhe h then use below
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 // require("dotenv").config({path:'./env'});
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/db.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERRR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
 
 /*
 

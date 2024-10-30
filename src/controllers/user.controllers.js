4ec83e514ff6eeac067b68d1cbfa528c8b20e5d8
Aditya Mehta <aditya0mehta1@gmail.com>
2024-10-30 23:48:01 +0530

    Add getWatchedHistory enpoints

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -426,7 +426,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     },
   ]);
 
-  console.log(channel);
+  console.log("channel in getUserChannelProfile -> ", channel);
 
   if (!channel?.lenght) {
     throw new ApiError(404, "channel does not exists");
@@ -436,6 +436,62 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     .status(200)
     .json(
       new ApiResponse(200, channel[0], "User channel is fetched successfully")
+    );
+});
+
+const getWatchedHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  console.log("user in getWatchedHistory:---> ", user);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0].watchHistory,
+        "Watch History fetched successfully"
+      )
     );
 });
 
@@ -450,4 +506,5 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getWatchedHistory,
 };

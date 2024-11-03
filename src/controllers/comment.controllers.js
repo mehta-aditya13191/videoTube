1cf2e05c5cb2d0a5eea429aa35e7ced466ed432e
Aditya Mehta <aditya0mehta1@gmail.com>
2024-11-04 01:01:45 +0530

    getVideoComments controllers is added

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -1,4 +1,4 @@
-import { isValidObjectId } from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Comment } from "../models/comment.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -34,10 +34,101 @@ const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  if (!videoId || !isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid videoId");
+  }
+
+  const pageNum = parseInt(page);
+  const limitNum = parseInt(limit);
+
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+              _id: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "comment",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesOnComment: {
+          $size: "$likes",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        likesOnComment: 1,
+        isLiked: 1,
+        owner: 1,
+        content: 1,
+        createdAt: 1,
+      },
+    },
+
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $skip: (pageNum - 1) * limitNum,
+    },
+    {
+      $limit: limitNum,
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, comments, "All comments are fetched successfully")
+    );
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+  // const { commentId } = req.params;
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
